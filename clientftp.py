4770f062733244f1ddf70b42c68fbc3032fa2e17
SPMatterhorn <62431902+SPMatterhorn@users.noreply.github.com>
2020-05-06 23:35:25 +0200

    Update clientftp.py
    Ajout d'une boucle While pour maintenir le script si il n'est pas demandé d'arrêter la connexion au serveur FTP
    Le choix arrêt de la connexion impose la condition de sortie de la boucle et l'arrêt du script

--- clientftp.py
+++ clientftp.py
@@ -22,19 +22,23 @@ print("Etat de la connexion:", etat); # état de la connexion
 rep=ftpObjet.dir(); 
 print(rep); # affichage du répertoire courant
 
-choix=int(input("que souhaitez vous faire ? (entrez le num puis enter) \n 1:créer un repertoire \n 2:supprimer un repertoire \n 3:renommer un fichier ou dossier \n 4:arrêter la connexion"));
-if choix==1:
-   dirname1 = input('taper le nom du repertoire à créer: ') # saisie du répertoire à créer
-   newrep= ftpObjet.mkd(dirname1) # création du nouveau répertoire
-if choix==2:
-   dirname2 = input('taper le nom du repertoire à supprimer: ') # saisie du répertoire à supprimer
-   delrep= ftpObjet.rmd(dirname2) # suppression du répertoire
-if choix==3:
-   filename_before = input('taper le nom du fichier ou repertoire à renommer: ') # saisie du fichier ou répertoire à renommer
-   filename_after = input('le renommer en: ') # saisie du nouveau nom de fichier ou répertoire
-   rename= ftpObjet.rename(filename_before,filename_after) # renommage du fichier ou répertoire
-if choix==4:
-   goodbye=ftpObjet.quit(); # arrêt de la connexion
-   print(goodbye);
+continuer_connexion = true;
+
+while continuer_connexion:
+   choix=int(input("que souhaitez vous faire ? (entrez le num puis enter) \n 1:créer un repertoire \n 2:supprimer un repertoire \n 3:renommer un fichier ou dossier \n 4:arrêter la connexion"));
+   if choix==1:
+      dirname1 = input('taper le nom du repertoire à créer: ') # saisie du répertoire à créer
+      newrep= ftpObjet.mkd(dirname1) # création du nouveau répertoire
+   if choix==2:
+      dirname2 = input('taper le nom du repertoire à supprimer: ') # saisie du répertoire à supprimer
+      delrep= ftpObjet.rmd(dirname2) # suppression du répertoire
+   if choix==3:
+      filename_before = input('taper le nom du fichier ou repertoire à renommer: ') # saisie du fichier ou répertoire à renommer
+      filename_after = input('le renommer en: ') # saisie du nouveau nom de fichier ou répertoire
+      rename= ftpObjet.rename(filename_before,filename_after) # renommage du fichier ou répertoire
+   if choix==4:
+      goodbye=ftpObjet.quit(); # arrêt de la connexion
+      print(goodbye);
+      continuer_connexion = false;
 
 
