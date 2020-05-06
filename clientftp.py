6f9aa446654ef69974001017f55ff59bfe7ac946
SPMatterhorn <62431902+SPMatterhorn@users.noreply.github.com>
2020-05-06 22:20:17 +0200

    Update clientftp.py
    Ajout du choix "Arrêter la connexion"

--- clientftp.py
+++ clientftp.py
@@ -11,7 +11,7 @@ from ftplib import FTP
 
 ftpObjet = FTP('pc2'); # connexion au serveur ftp pc2, port par défaut, ftpObjet est une instance de la classe FTP
 # ftpObjet = FTP(serverName);
-ftpReponse = ftpObjet.login(user='user',passwd='testtest') # identifiant, mot de passe
+ftpReponse = ftpObjet.login(user='user',passwd='testtest'); # identifiant, mot de passe
 print("Etat de l'identification: ", ftpReponse);
 # ftpObjet.login(utilisateur, motdepasse);
 ftpObjet.cwd('/home/user'); # changer de répertoire, se trouver dans le répertoire user
@@ -19,10 +19,10 @@ ftpObjet.cwd('/home/user'); # changer de répertoire, se trouver dans le répert
 etat=ftpObjet.getwelcome();
 print("Etat de la connexion:", etat); # état de la connexion
 
-rep=ftpObjet.dir() 
-print(rep) # affichage du répertoire courant
+rep=ftpObjet.dir(); 
+print(rep); # affichage du répertoire courant
 
-choix=input("que souhaitez vous faire ? (entrez le num puis enter) \n 1:créer un repertoire \n 2:supprimer un repertoire \n 3:renommer un fichier ou dossier \n 4:s")
+choix=input("que souhaitez vous faire ? (entrez le num puis enter) \n 1:créer un repertoire \n 2:supprimer un repertoire \n 3:renommer un fichier ou dossier \n 4:arrêter la connexion");
 if choix=="1":
    dirname1 = input('taper le nom du repertoire à créer: ') # saisie du répertoire à créer
    newrep= ftpObjet.mkd(dirname1) # création du nouveau répertoire
@@ -33,5 +33,8 @@ if choix=="3":
    filename_before = input('taper le nom du fichier ou repertoire à renommer: ') # saisie du fichier ou répertoire à renommer
    filename_after = input('le renommer en: ') # saisie du nouveau nom de fichier ou répertoire
    rename= ftpObjet.rename(filename_before,filename_after) # renommage du fichier ou répertoire
+if choix=="4":
+   goodbye=ftpObjet.quit(); # arrêt de la connexion
+   print(goodbye);
 
 
